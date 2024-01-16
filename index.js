e3bff782d128211b954972f51e27e8035090cd80
almasdahir <almas.dahir@student.moringaschool.com>
2024-01-16 12:06:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function isPalindrome(word) {
-  // Write your algorithm here
 }
+
+
+function isPalindrome(str) {
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  return cleanStr === cleanStr.split('').reverse().join('');
+}
+
+module.exports = isPalindrome;
+
 
 /* 
   Add your pseudocode here
